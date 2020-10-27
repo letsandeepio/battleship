87092f7af1d34fef9779638e730f575e8a5b87f0
letsandeepio <sandeepchopra7@gmail.com>
2020-10-27 12:41:15 -0400

    adds jest to the project

---
+++ components/Board.test.js
@@ -0,0 +1,14 @@
+const Board = require('./Board');
+
+describe('testing Board component ', () => {
+  test('successfully initialize the board', () => {
+    const board = new Board(8, 8);
+    expect(board.grid.length).toBe(8);
+  });
+  test('successfully print the board', () => {
+    //getPrintableGrid should return a string of length 171
+    const board = new Board(8, 8);
+    console.log(board.getPrintableGrid());
+    expect(board.getPrintableGrid().length).toBe(171);
+  });
+});
--- index.js
+++ index.js
@@ -1,42 +1,8 @@
-class Board {
-  constructor(width, height) {
-    this.height = height;
-    this.width = width;
-    this.grid = generateGrid(width, height);
-  }
-  getPrintableGrid() {
-    let printedGrid = '';
-    printedGrid += generateTopRow(this.width) + '\n';
-    for (let i = 0; i < this.height; i++) {
-      let rowStr = i + 1 + ' ';
-      for (let cell of this.grid[i]) {
-        rowStr += cell + ' ';
-      }
-      printedGrid += rowStr + '\n';
-    }
-    return printedGrid;
-  }
-}
-
-const generateGrid = (width, height) => {
-  let grid = [];
-  for (let i = 0; i < height; i++) {
-    grid[i] = [];
-    for (let j = 0; j < width; j++) {
-      grid[i][j] = '-';
-    }
-  }
-  return grid;
-};
-
-const generateTopRow = (width) => {
-  let topRow = '  ';
-  for (let i = 0; i < width; i++) {
-    topRow += i + 1 + ' ';
-  }
-  return topRow;
-};
+const Board = require('./components/Board');
 
 let board = new Board(8, 8);
 
 console.log(board.getPrintableGrid());
+
+console.log(board.isValidCoordinate({ x: 10, y: 10 }));
+console.log(board.isValidCoordinate({ x: 2, y: 2 }));
